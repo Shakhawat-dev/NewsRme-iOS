7a7bcf0048402bea7c62ea6befa3a63baf2652e5
Md. Shakhawat Hossain Shahin <shakhawat.ch@gmail.com>
2020-12-20 11:08:25 -0800

    Initial Commit

---
+++ Shared/NewsRmeApp.swift
@@ -0,0 +1,17 @@
+//
+//  NewsRmeApp.swift
+//  Shared
+//
+//  Created by Shakhawat Hossain Shahin on 20/12/20.
+//
+
+import SwiftUI
+
+@main
+struct NewsRmeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
